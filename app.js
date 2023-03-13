e72a8bdafacbe6f8673bd364bc9e8b492e02f24f
Benedict Meeske <benedict.meeske@gmail.com>
2023-03-13 23:00:39 +0100

    add basic grid layout in css with 16 rows and 16 cols, calculate number of rows and cols, to create griditems on page load

---
+++ app.js
@@ -0,0 +1,24 @@
+const container = document.querySelector(".container");
+const gridItem = document.querySelector(".griditem");
+
+// get computed style of our grid element
+const gridComputed = window.getComputedStyle(container);
+// get number of grid rows
+const gridRows = gridComputed.getPropertyValue("grid-template-rows").split(" ").length;
+//get number of grid columns
+const gridCols = gridComputed.getPropertyValue("grid-template-columns").split(" ").length;
+
+const gridCells = gridRows * gridCols;
+
+
+window.onload = createDivs();
+
+// creation of griditems(divs) to fill out each cell based on number of grid cells
+function createDivs() {
+    for (let i = 0; i < gridCells; i++) {
+        const div = document.createElement("div");
+        div.classList.add("griditem");
+        container.appendChild(div);
+        
+    }
+}
